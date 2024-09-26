87b1064c772c51828ccc3d655f2de7c1bbae089f
Myron <josephonyekachi77@gmail.com>
2024-09-26 15:07:12 +0100

    Added admin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,7 +52,7 @@ export const signin = async (req, res, next) => {
         }
 
         //create token for sign-in
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET)
 
         //remove the password from the valid user
         const { password: pass, ...rest } = validUser._doc;
@@ -75,7 +75,7 @@ export const google = async (req, res, next) => {
 
         if (user) {
             //create token for sign-in
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
 
             //remove the password from the valid user
             const { password, ...rest } = user._doc;
@@ -97,7 +97,7 @@ export const google = async (req, res, next) => {
             await newUser.save()
 
             //create token for sign-in
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET)
 
             //remove the password from the valid user
             const { password, ...rest } = user._doc;
