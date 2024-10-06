0bf4283bd0d1e075103ef5e6b5d57e667e7d5455
Myron <josephonyekachi77@gmail.com>
2024-10-06 19:52:31 +0100

    Displayed all user posts in the admin dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -38,11 +38,11 @@ export const getposts = async (req, res, next) => {
         const sortDirection = req.query.order === 'asc' ? 1 : -1
 
         const posts = await Post.find({
-            
+
             ...(req.query.userId && { userId: req.query.userId }),
             ...(req.query.category && { category: req.query.category }),
             ...(req.query.slug && { slug: req.query.slug }),
-            ...(req.query.postId && { postId: req.query.postId }),
+            ...(req.query.postId && { _id: req.query.postId }),
             ...(req.query.searchTerm && {
                 $or: [
                     { title: { $regex: req.query.searchTerm, $options: 'i' } },
