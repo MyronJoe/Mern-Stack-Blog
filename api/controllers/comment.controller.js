07e33bc3b75055650e9526bf07c33b7d830dd165
Myron <josephonyekachi77@gmail.com>
2024-12-23 11:39:08 +0100

    Added the edit comment function

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -40,13 +40,13 @@ export const likeComment = async (req, res, next) => {
     try {
         const comment = await Comment.findById(req.params.commentId)
 
-        if(!comment){
+        if (!comment) {
             return next(errorHandler(404, 'Comment not found'))
         }
 
         const userIndex = comment.likes.indexOf(req.user.id);
 
-        if(userIndex === -1){
+        if (userIndex === -1) {
             comment.numberOfLikes += 1;
             comment.likes.push(req.user.id)
         } else {
@@ -60,4 +60,31 @@ export const likeComment = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const editComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'))
+        }
+
+        if (comment.userId !== req.user.id && !req.user.isAdmin === 'false') {
+            return next(errorHandler(403, 'You are not allowed to edit this comment'))
+        }
+
+        const editComment = await Comment.findByIdAndUpdate(
+            req.params.commentId,
+            {
+                content: req.body.content,
+            },
+            { new: true }
+        )
+
+        res.status(200).json(editComment)
+
+    } catch (error) {
+        next(error)
+    }
+
 }
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -87,6 +87,14 @@ export default function CommentSection({ postId }) {
         }
     };
 
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+            comments.map((c) =>
+                c._id === comment._id ? { ...c, content: editedContent } : c
+            )
+        );
+    };
+
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
             {currentUser ? (
@@ -157,6 +165,7 @@ export default function CommentSection({ postId }) {
                             key={comment._id}
                             comment={comment}
                             onLike={handleLike}
+                            onEdit={handleEdit}
                         />
                     ))}
                 </>
