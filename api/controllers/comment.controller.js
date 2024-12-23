78b93ecd80a1832e83e907baad8bbb94c91e4f6c
Myron <josephonyekachi77@gmail.com>
2024-12-23 12:09:05 +0100

    Added the comment delete functionality

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,7 +1,7 @@
 import Comment from "../models/comment.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createComment = async (res, req, next) => {
+export const createComment = async (req, res, next) => {
 
     try {
         const { content, postId, userId } = req.body;
@@ -13,11 +13,11 @@ export const createComment = async (res, req, next) => {
             content,
             postId,
             userId,
-        })
+        });
 
-        await newComment.save()
+        await newComment.save();
 
-        res.status(200).json(newComment)
+    res.status(200).json(newComment);
 
     } catch (error) {
         next(error)
@@ -88,3 +88,21 @@ export const editComment = async (req, res, next) => {
     }
 
 }
+
+export const deleteComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'));
+        }
+        if (comment.userId !== req.user.id && !req.user.isAdmin) {
+            return next(
+                errorHandler(403, 'You are not allowed to delete this comment')
+            );
+        }
+        await Comment.findByIdAndDelete(req.params.commentId);
+        res.status(200).json('Comment has been deleted');
+    } catch (error) {
+        next(error);
+    }
+};
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
 
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
@@ -128,7 +128,7 @@ export default function Comment({ comment, onLike, onEdit }) {
                                             </button>
                                             <button
                                                 type='button'
-                                                // onClick={() => onDelete(comment._id)}
+                                                onClick={() => onDelete(comment._id)}
                                                 className='text-gray-400 hover:text-red-500'
                                             >
                                                 Delete
